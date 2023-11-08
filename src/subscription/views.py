d200ca98b3f789065a6dc25086ff5fca1ed46c70
minwoo <minwoo9621@gmail.com>
2023-11-08 20:58:39 +0900

    Add view for subscription

--- src/subscription/views.py
+++ src/subscription/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+from subscription.serializers import SubscriptionRequestSerializer
+from subscription.services import SubscriptionService
+from subscription.unit_of_work import DjangoUnitOfWork
+
+
+class SubscriptionViewSet(viewsets.ViewSet):
+    uow = DjangoUnitOfWork()
+    subscription_service = SubscriptionService(uow)
+
+    @action(detail=False, methods=["post"], url_path="subscribe")
+    def subscribe(self, request):
+        serializer = SubscriptionRequestSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        valid_data = serializer.validated_data
+
+        try:
+            result = self.subscription_service.subscribe_user_to_plan(
+                user_id=request.user.id,
+                plan_name=valid_data["plan_name"],
+                payment_details=valid_data["payment_details"],
+            )
+        except ValueError as e:
+            return Response(
+                {"success": False, "message": str(e)},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        return Response(result, status=status.HTTP_200_OK)
