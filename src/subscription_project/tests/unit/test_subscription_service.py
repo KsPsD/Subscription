080b0286cd59d63e2e78cd60a815042d53efad09
minwoo <minwoo9621@gmail.com>
2023-11-09 21:45:39 +0900

    Add test case for renew and cancel subscription

--- src/subscription_project/tests/unit/test_subscription_service.py
+++ src/subscription_project/tests/unit/test_subscription_service.py
@@ -1,8 +1,14 @@
+from datetime import date
 from unittest import mock
 
 from django.test import TestCase
 
-from subscription.domain_models import PaymentMethodType, SubscriptionPlan
+from subscription.domain_models import (
+    PaymentMethodType,
+    SubscriptionPlan,
+    SubscriptionStatus,
+    UserSubscription,
+)
 from subscription.services import SubscriptionService
 from subscription.unit_of_work import DjangoUnitOfWork
 from subscription_project.tests.unit.fake import FakeUnitOfWork
@@ -60,3 +66,58 @@ class SubscribeUserToPlanTestCase(TestCase):
 
             self.assertFalse(self.uow.committed)
             self.assertTrue(self.uow.rolled_back)
+
+    def test_cancel_subscription_success(self):
+        self.uow.user_subscriptions.add(
+            UserSubscription(
+                user_id=self.user_id,
+                plan_id=1,
+                start_date=date.today(),
+                end_date=date.today(),
+                status=SubscriptionStatus.ACTIVE,
+            )
+        )
+
+        response = self.service.cancel_subscription(user_id=self.user_id)
+
+        self.assertTrue(response["success"])
+        self.assertEqual(
+            f"User {self.user_id} has cancelled subscription successfully.",
+            response["message"],
+        )
+        self.assertTrue(self.uow.committed)
+
+    def test_cancel_subscription_with_invalid_user(self):
+        with self.assertRaises(ValueError) as context:
+            self.service.cancel_subscription(user_id=123)
+
+            self.assertIsInstance(context, ValueError)
+
+            self.assertFalse(self.uow.committed)
+            self.assertTrue(self.uow.rolled_back)
+
+    def test_renew_subscription_success(self):
+        self.uow.subscription_plans.add(
+            SubscriptionPlan(
+                id=1,
+                name=self.plan_name,
+                price=10.00,
+                payment_cycle="monthly",
+                description="Basic Plan",
+                duration_days=30,
+            )
+        )
+        self.uow.user_subscriptions.add(
+            UserSubscription(
+                user_id=self.user_id,
+                plan_id=1,
+                start_date=date.today(),
+                end_date=date.today(),
+                status=SubscriptionStatus.ACTIVE,
+            )
+        )
+
+        response = self.service.renew_subscription(user_id=self.user_id)
+
+        self.assertTrue(response["success"])
+        self.assertTrue(self.uow.committed)
