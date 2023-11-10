9570977593b6dc5b310927f7bf31aff3303f97e2
minwoo <minwoo9621@gmail.com>
2023-11-10 20:41:43 +0900

    Add changing subscription plan logic

--- src/subscription/services.py
+++ src/subscription/services.py
@@ -162,9 +162,69 @@ class SubscriptionService:
             )
             self.uow.user_subscriptions.add(new_subscription)
 
-            # NOTE: 결제 정보 생성 및 추가하는 로직 (여기서는 생략)
-
             return {
                 "success": True,
                 "message": "Subscription renewed successfully.",
             }
+
+    def _calculate_proration(
+        self, current_subscription: UserSubscription, new_plan: SubscriptionPlan
+    ):
+        remaining_days = (current_subscription.end_date - date.today()).days
+
+        # 일일 요금 계산
+        daily_rate_current_plan = (
+            current_subscription.plan.price / current_subscription.plan.duration_days
+        )
+        daily_rate_new_plan = new_plan.price / new_plan.duration_days
+
+        # 비례 조정된 금액 계산
+        prorated_amount = (
+            daily_rate_new_plan - daily_rate_current_plan
+        ) * remaining_days
+
+        return prorated_amount, remaining_days
+
+    def change_subscription_plan(self, user_id: int, new_plan_name: str):
+        with self.uow:
+            try:
+                current_subscription = (
+                    self.uow.user_subscriptions.get_active_subscription_by_user_id(
+                        user_id
+                    )
+                )
+                new_plan = self.uow.subscription_plans.get(new_plan_name)
+
+                prorated_amount, remaining_days = self._calculate_proration(
+                    current_subscription, new_plan
+                )
+
+                payment_success, payment_details = self._process_payment(
+                    user_id, prorated_amount
+                )
+                if not payment_success:
+                    return {
+                        "success": False,
+                        "message": "Failed to process payment for plan change.",
+                    }
+
+                current_subscription.end_date = date.today()
+                current_subscription.status = SubscriptionStatus.CANCELED
+                self.uow.user_subscriptions.update(current_subscription)
+
+                new_subscription = UserSubscription(
+                    user_id=user_id,
+                    plan=new_plan,
+                    start_date=date.today(),
+                    end_date=date.today() + timedelta(days=remaining_days),
+                    status=SubscriptionStatus.ACTIVE,
+                )
+                self.uow.user_subscriptions.add(new_subscription)
+
+                return {
+                    "success": True,
+                    "message": "Subscription plan changed successfully.",
+                }
+
+            except ValueError as e:
+                raise e
