6809a2c676df2065dcefcf356ca4a4f91e39bdf9
minwoo <minwoo9621@gmail.com>
2023-11-13 21:05:44 +0900

    Add test case for handlers

--- src/subscription_project/tests/unit/test_handlers.py
+++ src/subscription_project/tests/unit/test_handlers.py
@@ -1,21 +1,22 @@
+import random
 from datetime import date
 from unittest import mock
 
 from django.test import TestCase
 
+from subscription.domain import commands
 from subscription.domain.domain_models import (
     PaymentMethodType,
     SubscriptionPlan,
     SubscriptionStatus,
     UserSubscription,
 )
-from subscription.domain.events import PaymentFailed
+from subscription.service_layer import message_bus
 from subscription.service_layer.services import SubscriptionService
-from subscription.service_layer.unit_of_work import DjangoUnitOfWork
 from subscription_project.tests.unit.fake import FakeUnitOfWork
 
 
-class SubscribeUserToPlanTestCase(TestCase):
+class TestCreateSubscription(TestCase):
     def setUp(self):
         self.user_id = 1
         self.plan_name = "Basic"
@@ -45,28 +46,28 @@ class SubscribeUserToPlanTestCase(TestCase):
                 duration_days=30,
             )
         )
-
-        response = self.service.subscribe_user_to_plan(
-            user_id=self.user_id,
-            plan_name=self.plan_name,
-            payment_details=self.payment_details,
+        results = message_bus.handle(
+            commands.CreateSubscription(
+                self.user_id,
+                self.plan_name,
+                self.payment_details,
+            ),
+            self.uow,
         )
-
+        response = results.pop()
         self.assertTrue(response["success"])
         self.assertIn("has subscribed to Basic plan successfully", response["message"])
         self.assertTrue(self.uow.committed)
 
     def test_subscribe_user_to_plan_with_invalid_plan(self):
         with self.assertRaises(ValueError) as context:
-            self.service.subscribe_user_to_plan(
-                user_id=123,
-                plan_name="NonExistentPlan",
-                payment_details={
-                    "method_type": PaymentMethodType.CREDIT_CARD.value,
-                    "card_number": "4242-4242-4242-4242",
-                    "expiration_date": "12/25",
-                    "cvc": "123",
-                },
+            results = message_bus.handle(
+                commands.CreateSubscription(
+                    self.user_id,
+                    self.plan_name,
+                    self.payment_details,
+                ),
+                self.uow,
             )
 
             self.assertIsInstance(context, ValueError)
@@ -75,17 +76,32 @@ class SubscribeUserToPlanTestCase(TestCase):
             self.assertTrue(self.uow.rolled_back)
 
     def test_cancel_subscription_success(self):
-        self.uow.user_subscriptions.add(
-            UserSubscription(
-                user_id=self.user_id,
-                plan=self.plan,
-                start_date=date.today(),
-                end_date=date.today(),
-                status=SubscriptionStatus.ACTIVE,
+        self.uow.subscription_plans.add(
+            SubscriptionPlan(
+                name=self.plan_name,
+                price=10.00,
+                payment_cycle="monthly",
+                description="Basic Plan",
+                duration_days=30,
             )
         )
+        message_bus.handle(
+            commands.CreateSubscription(
+                self.user_id,
+                self.plan_name,
+                self.payment_details,
+            ),
+            self.uow,
+        )
 
-        response = self.service.cancel_subscription(user_id=self.user_id)
+        results = message_bus.handle(
+            commands.CancelSubscription(
+                self.user_id,
+            ),
+            self.uow,
+        )
+
+        response = results.pop()
 
         self.assertTrue(response["success"])
         self.assertEqual(
@@ -96,7 +112,12 @@ class SubscribeUserToPlanTestCase(TestCase):
 
     def test_cancel_subscription_with_invalid_user(self):
         with self.assertRaises(ValueError) as context:
-            self.service.cancel_subscription(user_id=123)
+            results = message_bus.handle(
+                commands.CancelSubscription(
+                    self.user_id,
+                ),
+                self.uow,
+            )
 
             self.assertIsInstance(context, ValueError)
 
@@ -116,9 +137,15 @@ class SubscribeUserToPlanTestCase(TestCase):
         )
 
         with mock.patch.object(
-            self.service, "_process_payment", return_value=(True, {})
+            self.service, "process_payment", return_value=(True, {})
         ) as mock_method:
-            response = self.service.renew_subscription(user_id=self.user_id)
+            results = message_bus.handle(
+                commands.RenewSubscription(
+                    self.user_id,
+                ),
+                self.uow,
+            )
+            response = results.pop()
 
             self.assertTrue(response["success"])
             self.assertTrue(self.uow.committed)
@@ -144,12 +171,15 @@ class SubscribeUserToPlanTestCase(TestCase):
             )
         )
 
-        with mock.patch.object(
-            self.service, "_process_payment", return_value=(True, {})
-        ) as mock_method:
-            response = self.service.change_subscription_plan(
-                user_id=self.user_id, new_plan_name="Premium"
+        with mock.patch.object(random, "choice", return_value=True) as mock_method:
+            results = message_bus.handle(
+                commands.ChangeSubscriptionPlan(
+                    self.user_id,
+                    "Premium",
+                ),
+                self.uow,
             )
+            response = results.pop()
 
             self.assertTrue(response["success"])
             self.assertTrue(self.uow.committed)
@@ -175,35 +205,17 @@ class SubscribeUserToPlanTestCase(TestCase):
             )
         )
 
-        with mock.patch.object(
-            self.service, "_process_payment", return_value=(False, {})
-        ) as mock_method:
-            response = self.service.change_subscription_plan(
-                user_id=self.user_id, new_plan_name="Premium"
+        with mock.patch.object(random, "choice", return_value=False) as mock_method:
+            results = message_bus.handle(
+                commands.ChangeSubscriptionPlan(
+                    self.user_id,
+                    "Premium",
+                ),
+                self.uow,
             )
+            response = results.pop()
 
             self.assertFalse(response["success"])
             self.assertEqual(
                 response["message"], "Failed to process payment for plan change."
             )
-
-    @mock.patch("random.choice")
-    def test_process_payment_with_mark_failed(self, mock_random_choice):
-        self.uow.subscription_plans.add(self.plan)
-        self.uow.user_subscriptions.add(
-            UserSubscription(
-                user_id=self.user_id,
-                plan=self.plan,
-                start_date=date.today(),
-                end_date=date.today(),
-                status=SubscriptionStatus.ACTIVE,
-            )
-        )
-        mock_random_choice.return_value = False
-
-        response = self.service.process_payment(
-            user_id=self.user_id,
-            amount=10.00,
-        )
-        self.assertFalse(response[0])
-        self.assertEqual(type(response[1].events[0]), PaymentFailed)
